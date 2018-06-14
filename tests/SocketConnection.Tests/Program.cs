502a5dd3b5ad82723a3e5cbc0ff3f1f93a868ae0
Nuno Araujo <nunobaraujo@hotmail.com>
2018-06-14 16:56:16 +0100

    Added Session and User Tests

--- tests/SocketConnection.Tests/Program.cs
+++ tests/SocketConnection.Tests/Program.cs
@@ -12,15 +12,40 @@ namespace SocketConnection.Tests
             System.Threading.Thread.Sleep(5000);
             Task.Run(async () => await RunTests()).Wait();
             Console.WriteLine("Tests Finished");
+            Console.ReadKey();
         }
 
         static async Task RunTests()
         {
-            IRestClient cli = new SocketClient.Client("localhost", 9865, null);
-            
+            IRestClient cli = new SocketClient.Client("nem", 9865, null);
+
             var token = await cli.SessionApi.LogIn(new LogInRequest { UserName = "sa", UserPassword = "#Na123" });
             Console.WriteLine(token);
+            await RunSessionTests(cli, token);
 
+
+            await cli.SessionApi.LogOut(new BearerTokenRequest { Token = token });
+        }
+
+        private static async Task RunSessionTests(IRestClient cli, string token)
+        {
+            var companyId = await cli.SessionApi.GetActiveCompany(new BearerTokenRequest { Token = token });
+            Console.WriteLine("{0} GetActiveCompany 1 = {1}", nameof(RunSessionTests), companyId);
+            await cli.SessionApi.SetActiveCompany(new IdRequest { Token = token, Id = "3" });
+            var newCompanyId = await cli.SessionApi.GetActiveCompany(new BearerTokenRequest { Token = token });
+            Console.WriteLine("{0} GetActiveCompany 2 = {1}", nameof(RunSessionTests), newCompanyId);
+            await cli.SessionApi.SetActiveCompany(new IdRequest { Token = token, Id = companyId });
+        }
+
+        private static async Task RunUserTests(IRestClient cli, string token)
+        {
+            var testUser = await cli.UserApi.Add(new CreateUserRequest
+            {
+                Email = "Test@test.com",
+                UserName = Guid.NewGuid().ToString().Replace("-", ""),
+                UserPassword = "SomePass"
+            });
+            Console.WriteLine("{0} Added Userr = {1}", nameof(RunUserTests), testUser.UserName);
         }
     }
 }
